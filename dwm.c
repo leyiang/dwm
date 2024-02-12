77e7462df4f79392cc13e00f954e39196e6527e9
Yiang <yiangforwork@gmail.com>
2024-02-13 02:42:46 +0800

    update: hide layout indicator

--- dwm.c
+++ dwm.c
@@ -846,9 +846,11 @@ drawbar(Monitor *m)
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		x += w;
 	}
-	w = TEXTW(m->ltsymbol);
-	drw_setscheme(drw, scheme[SchemeTagsNorm]);
-	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
+
+    // Hide layout indicator
+	// w = TEXTW(m->ltsymbol);
+	// drw_setscheme(drw, scheme[SchemeTagsNorm]);
+	// x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = m->ww - tw - stw - x) > bh) {
 		if (m->sel) {
